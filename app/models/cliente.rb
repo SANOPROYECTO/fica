b53e63ac8019faa44ccf1d7bdcb35b1f310c55e7
williamrojas <wdrojas8@misena.edu.co>
2015-05-06 22:03:40 -0500

    val

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -1,6 +1,15 @@
 class Cliente < ActiveRecord::Base
 	def self.search(search, page)
-	where(['upper(nombre) like ?',
-	"%#{search}%".upcase]).paginate(page: page, per_page: 5).order("nombre")
-end
+	 where(['upper(nombre) like ?',
+	 "%#{search}%".upcase]).paginate(page: page, per_page: 5).order("nombre")
+    end
+    # Validar que los atributos sean obligatorios
+	validates :nombre, :presence => true
+	validates :direccion, :presence => true
+	validates :hora, :presence => true
+	validates :valor, :presence => true
+	# valida la longitud de un campo, tanto la longitud minima como la maxima
+	validates :nombre, length: { minimum: 5, maximum: 50 }
+	# Valida si un atributo es numerico
+validates :valor, numericality: true
 end
