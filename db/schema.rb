73b8c6a2e36e21a0cfcb44eda8b9004fbd77ab5b
williamrojas <wdrojas8@misena.edu.co>
2015-06-11 15:20:08 -0500

    3

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150505005851) do
 
-# Could not dump table "clientes" because of following NoMethodError
-#   undefined method `[]' for nil:NilClass
+  create_table "clientes", force: :cascade do |t|
+    t.string   "nombre"
+    t.string   "telefono"
+    t.string   "celular"
+    t.string   "barrio"
+    t.string   "direccion"
+    t.string   "referenciadir"
+    t.date     "fecha"
+    t.string   "hora"
+    t.string   "ocacion"
+    t.integer  "valor"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
 
 end
